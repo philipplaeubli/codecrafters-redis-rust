b66d67375b13076d2f57ca2aad8c11153288f2c4
philipplaeubli <philipp.laeubli@dreipol.ch>
2025-12-23 13:10:49 +0100

    Convert to async Tokio TCP server
    Switch to an async Tokio runtime and handle each client in its own task.
    Replace the blocking loop with an async accept loop and per-connection
    Handlers use async read/write to process data

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,48 @@
 #![allow(unused_imports)]
 use std::{
     io::{BufRead, BufReader, Read, Write},
+    thread,
+};
+
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_stream(mut stream: &TcpStream) -> Result<(), std::io::Error> {
+async fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let mut buffer = [0u8; 128];
     loop {
-        let mut buffer = [0; 128];
-        stream.read(&mut buffer)?;
-        stream.write_all(b"+PONG\r\n")?;
-        println!("Sent PONG");
+        let result = stream.read(&mut buffer).await?;
+        if result == 0 {
+            // Connection closed by client
+            println!("Connection closed by client");
+            return Ok(());
+        } else if result > 0 {
+            // don't care if we got some error, just send PONG
+            stream.write_all(b"+PONG\r\n").await?;
+            println!("Sent PONG");
+        }
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Bind the TCP listener to the specified address and port
     let addr = std::env::var("REDIS_ADDR").unwrap_or_else(|_| "127.0.0.1:6379".to_string());
-
     let tcp_binding = TcpListener::bind(&addr);
+    let tcp_listener = tcp_binding.await?;
+    println!("Listening on {} - awaiting connections", addr);
+    loop {
+        let (stream, _addr) = tcp_listener.accept().await?;
+        println!("Accepted connection from client");
 
-    if let Ok(listener) = tcp_binding {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(incoming_stream) => {
-                    println!("accepted new connection on {}", addr);
-                    let _ = handle_stream(&incoming_stream);
-                }
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                eprintln!("Error handling connection: {e}");
             }
-        }
-    } else {
-        panic!("Failed to bind to port");
+        });
     }
 }
